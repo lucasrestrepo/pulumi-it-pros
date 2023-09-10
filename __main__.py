a2951127d0f8cf8d33de207324971a8972335a38
lrestrepoteck <lucas.restrepo@teck.com>
2023-09-10 17:51:07 -0500

    add blob container

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ from pulumi_azure_native import storage
 from pulumi_azure_native import resources
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup("resource_group")
+resource_group = resources.ResourceGroup("resource_group",)
 
 # Create an Azure resource (Storage Account)
 account = storage.StorageAccount(
@@ -18,14 +18,4 @@ account = storage.StorageAccount(
 )
 
 # Export the primary key of the Storage Account
-primary_key = (
-    pulumi.Output.all(resource_group.name, account.name)
-    .apply(
-        lambda args: storage.list_storage_account_keys(
-            resource_group_name=args[0], account_name=args[1]
-        )
-    )
-    .apply(lambda accountKeys: accountKeys.keys[0].value)
-)
-
-pulumi.export("primary_storage_key", primary_key)
+container = storage.BlobContainer(account.name, "blob1", resource_group.name)
